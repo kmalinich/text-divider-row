f171520fe5713eb1fd3b4262d8d2c86a2834044a
kdm <kennygprs@gmail.com>
2021-12-10 10:46:38 -0500

    Few styling tweaks, more CSS variables, update devDependencies and rollup configs, bring in my personal eslint rules

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,23 @@
-import resolve from "rollup-plugin-node-resolve";
-import typescript from "rollup-plugin-typescript2";
-import babel from "rollup-plugin-babel";
-import { terser } from "rollup-plugin-terser";
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
-  input: ["src/text-divider-row.ts"],
-  output: {
-    dir: "./dist",
-    format: "es",
-  },
-  plugins: [
-    resolve(),
-    typescript(),
-    babel({
-      exclude: "node_modules/**",
-    }),
-    terser(),
-  ],
+	input  : [ 'src/text-divider-row.ts' ],
+
+	output : {
+		dir    : './dist',
+		format : 'es',
+	},
+
+	plugins : [
+		resolve(),
+		typescript(),
+		babel({
+			babelHelpers : 'bundled',
+			exclude      : 'node_modules/**',
+		}),
+		terser(),
+	],
 };
--- src/text-divider-row.ts
+++ src/text-divider-row.ts
@@ -1,101 +1,108 @@
-import { LitElement, html, customElement, property, TemplateResult, css, CSSResult } from 'lit-element';
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { LitElement, CSSResult, TemplateResult, css, html } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
 import { TextDividerRowConfig } from './types';
 import { CARD_VERSION } from './const';
 
 /* eslint no-console: 0 */
 console.info(
-  `%c  TEXT-DIVIDER-ROW  \n%c  Version ${CARD_VERSION}     `,
-  'color: orange; font-weight: bold; background: black',
-  'color: white; font-weight: bold; background: dimgray',
+	`%c  TEXT-DIVIDER-ROW  \n%c  Version ${CARD_VERSION}     `,
+	'color: orange; font-weight: bold; background: black',
+	'color: white; font-weight: bold; background: dimgray'
 );
 
 @customElement('text-divider-row')
+
 class TextDividerRow extends LitElement {
-  @property() private _config?: TextDividerRowConfig;
+	@property() private _config?: TextDividerRowConfig;
 
-  public setConfig(config: TextDividerRowConfig): void {
-    if (!config || !config.text) {
-      throw new Error('Error in card configuration.');
-    }
+	public setConfig(config: TextDividerRowConfig): void {
+		if (!config || !config.text) {
+			throw new Error('Error in card configuration.');
+		}
 
-    this._config = config;
-  }
+		this._config = config;
+	}
 
-  protected getClass(): string {
-    const allowedValues = ['center', 'left', 'right'];
-    if (this._config && this._config.align) {
-      if (allowedValues.includes(this._config.align)) {
-        return `text-divider text-divider-${this._config.align}`;
-      }
-    }
-    return 'text-divider text-divider-center';
-  }
+	protected getClass(): string {
+		const allowedValues = [ 'center', 'left', 'right' ];
+		if (this._config && this._config.align) {
+			if (allowedValues.includes(this._config.align)) {
+				return `text-divider text-divider-${this._config.align}`;
+			}
+		}
 
-  protected render(): TemplateResult | void {
-    if (!this._config) {
-      return html``;
-    }
+		return 'text-divider text-divider-center';
+	}
 
-    return html`
-      <div class="text-divider-container">
-        <h2 class="${this.getClass()}">
-          <span>${this._config.text}</span>
-        </h2>
-      </div>
-    `;
-  }
+	protected render(): TemplateResult | void {
+		if (!this._config) {
+			return html``;
+		}
 
-  static get styles(): CSSResult {
-    return css`
-      :host {
-        display: block;
-        position: relative;
-        --background: var(--ha-card-background, var(--card-background-color));
-        --divider-color: var(--text-divider-color, var(--secondary-text-color));
-        --font-size: var(--text-divider-font-size, 14px);
-        --line-size: var(--text-divider-line-size, 1px);
-      }
+		return html`
+			<div class="text-divider-container">
+				<h2 class="${this.getClass()}">
+					<span>${this._config.text}</span>
+				</h2>
+			</div>
+		`;
+	}
 
-      .text-divider {
-        width: 100%;
-        border-bottom: var(--line-size) solid var(--divider-color);
-        line-height: 0;
-        margin: 10px 0 20px;
-      }
+	static get styles(): CSSResult {
+		return css`
+			:host {
+				display: block;
+				position: relative;
+				--background: var(--ha-card-background, var(--card-background-color));
+				--divider-color: var(--text-divider-color, var(--secondary-text-color));
+				--font-size: var(--text-divider-font-size, 14px);
+				--line-size: var(--text-divider-line-size, 1px);
+			}
 
-      .text-divider-container {
-        margin: var(--text-divider-margin, 1em 0);
-      }
+			.text-divider {
+				border-bottom: var(--line-size) solid var(--divider-color);
+				line-height: 0;
+				margin: var(--text-divider-margin, 1em 0);
+				width: 100%;
+			}
 
-      .text-divider-center {
-        text-align: center;
-      }
+			.text-divider-container {
+				margin: var(--text-divider-margin, 1em 0);
+			}
 
-      .text-divider-left {
-        text-align: left;
-      }
+			.text-divider-center {
+				text-align: center;
+			}
 
-      .text-divider-right {
-        text-align: right;
-      }
+			.text-divider-left {
+				text-align: left;
+			}
 
-      .text-divider span {
-        color: var(--divider-color);
-        font-size: var(--font-size);
-        background: var(--background);
-        padding: 1px 1em;
-      }
-      .text-divider-center span {
-        margin: 0px;
-      }
+			.text-divider-right {
+				text-align: right;
+			}
 
-      .text-divider-left span {
-        margin: 0 0 0 1em;
-      }
+			.text-divider span {
+				background: var(--primary-background-color);
+				color: var(--divider-color);
+				font-size: var(--font-size);
+				padding: var(--text-divider-padding, 1em);
+			}
 
-      .text-divider-right span {
-        margin: 0 1em 0 0;
-      }
-    `;
-  }
+			.text-divider-center span {
+				margin: 0px;
+			}
+
+			.text-divider-left span {
+				margin: var(--text-divider-left-margin, 0 0 0 1em);
+			}
+
+			.text-divider-right span {
+				margin: var(--text-divider-right-margin, 0 1em 0 0);
+			}
+		`;
+	}
 }
